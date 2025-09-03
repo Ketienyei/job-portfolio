9db9c24c95dfa9c36a9cd53aa76219242b986c8f
Cynthia Ketienyei <108278881+Ketienyei@users.noreply.github.com>
2025-09-03 22:26:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -128,4 +128,24 @@ function revealOnScroll() {
     }
   });
 
-  
+const backToTopBtn = document.getElementById("backToTop");
+
+  // Show button when scrolled down
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) { 
+      backToTopBtn.style.display = "block";
+    } else {
+      backToTopBtn.style.display = "none";
+    }
+  });
+
+  // Scroll to top when clicked
+  backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });  
+
+
+
